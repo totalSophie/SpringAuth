8ff2185f0a9e172c9dde7c51fb99bf938432f92f
Sophie <totalsophie@gmail.com>
2023-06-25 18:40:05 +0300

    Add the getSignInKey Method

--- src/main/java/com/example/jwtspring/user/config/JwtService.java
+++ src/main/java/com/example/jwtspring/user/config/JwtService.java
@@ -1,10 +1,29 @@
 package com.example.jwtspring.user.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+    private static final String SECRETKEY = "kKlV3JV8NhnjDz6iD1sRjQAsTMESp3gP";
     public String extractUsername(String token) {
         return null;
+    }
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder().setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRETKEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
