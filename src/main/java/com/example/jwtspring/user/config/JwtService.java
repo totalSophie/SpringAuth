2f8091ad043f38cc1f2da97446011792fc49564b
Sophie <totalsophie@gmail.com>
2023-06-25 19:53:22 +0300

    Add the checkExpiration  method

--- src/main/java/com/example/jwtspring/user/config/JwtService.java
+++ src/main/java/com/example/jwtspring/user/config/JwtService.java
@@ -44,10 +44,20 @@ public class JwtService {
                 .compact();
 
     }
-    public boolean isTokenValid(String token, UserDetails userDetails()
+    public boolean isTokenValid(String token, UserDetails userDetails)
     {
-
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && isTokenExpired(token));
     }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
     private Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRETKEY);
         return Keys.hmacShaKeyFor(keyBytes);
