dcb5eea2127272f47cb3cefba1a5f6b103f82f71
Sophie <totalsophie@gmail.com>
2023-06-25 19:47:05 +0300

    Add the generate token method

--- src/main/java/com/example/jwtspring/user/config/JwtService.java
+++ src/main/java/com/example/jwtspring/user/config/JwtService.java
@@ -2,11 +2,15 @@ package com.example.jwtspring.user.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -26,7 +30,24 @@ public class JwtService {
                 .parseClaimsJws(token)
                 .getBody();
     }
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails)
+    {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
 
+    }
+    public boolean isTokenValid(String token, UserDetails userDetails()
+    {
+
+    }
     private Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRETKEY);
         return Keys.hmacShaKeyFor(keyBytes);
