006c4fba83c8c15813e51bdfa07846f3b9ad955b
Sophie <totalsophie@gmail.com>
2023-06-25 19:09:52 +0300

    Implement function to extract a single claim

--- src/main/java/com/example/jwtspring/user/config/JwtService.java
+++ src/main/java/com/example/jwtspring/user/config/JwtService.java
@@ -7,12 +7,17 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
     private static final String SECRETKEY = "kKlV3JV8NhnjDz6iD1sRjQAsTMESp3gP";
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims =extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
     private Claims extractAllClaims(String token){
         return Jwts
